13a8b4a4463242370d6291e7cc5d846042da2cb9
jonathanlim128 <heilim@ucdavis.edu>
2023-02-27 14:43:04 -0800

    modified how dates are selected

--- pocketASD/screens/SchedulingScreen.js
+++ pocketASD/screens/SchedulingScreen.js
@@ -4,27 +4,32 @@ import { Calendar } from 'react-native-calendars';
 
 const SchedulingScreen = () => {
 
-  const [selectedDates, setSelectedDates] = useState([]);
+  const [selectedDates, setSelectedDates] = useState(new Set());
   const [markedDates, setMarkedDates] = useState({});
   const date = new Date();
   const today = date.getFullYear() + '-0' + (date.getMonth() + 1) + '-' + date.getDate();
 
   function updateSelectedDates(date){
     let dates = selectedDates;
-    if(dates.length >= 3){
-      dates = selectedDates.slice(1);
+    if (dates.has(date)){
+      dates.delete(date)
+    }else if(dates.size < 3){
+      dates.add(date)
     }
-    dates.push(date)
     //console.log(dates)
     setSelectedDates(dates)
     const marked = {};
-    dates.forEach(date => (Object.assign(marked, {[date]: {selected: true, selectedColor: 'blue'}})))
+    dates.forEach(date => (Object.assign(marked, {[date]: {selected: true, selectedColor: 'blue', disabled: false}})))
     //console.log(marked)
     setMarkedDates(marked)
   }
 
   function confirmDates(){
     console.log(selectedDates)
+  }
+
+  function disableDays(){
+    return selectedDates.size >= 3
   }
 
   return (
@@ -36,6 +41,8 @@ const SchedulingScreen = () => {
           updateSelectedDates(date.dateString);
         }}
         markedDates={markedDates}
+        disabledByDefault={disableDays()}
+        disableAllTouchEventsForDisabledDays={disableDays()}
       />
       <Button onPress={confirmDates} title="Confirm Dates"/>
   	</View>
