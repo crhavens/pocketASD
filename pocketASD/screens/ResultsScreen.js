bfbd8f454374b1bc571bb9b07f088cdc9d3dff3a
Aaron <aaronmwong0820@gmail.com>
2023-02-27 13:25:42 -0800

    Results show on result screen

--- pocketASD/screens/ResultsScreen.js
+++ pocketASD/screens/ResultsScreen.js
@@ -6,9 +6,20 @@ import {
   TouchableOpacity
 } from "react-native";
 import { useNavigation } from '@react-navigation/native'
+import questions from '../data/questions'
 
 const ResultsScreen = ({ route }) => {
   const navigation = useNavigation()
+  const data = questions
+  let score = 0
+  for(let i = 0; i < 20; i++) {
+    if(route.params.answers[i] == "Yes") {
+      score += data[i].options.Yes
+    }
+    else {
+      score += data[i].options.No
+    }
+  }
 
   return (
     <View style={{padding:10}}>
@@ -19,7 +30,7 @@ const ResultsScreen = ({ route }) => {
           padding:10
         }}
       >
-      Your results:
+      Your Score: {score}
       </Text>
     </View>
   )
